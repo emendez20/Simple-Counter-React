1a08d6b68a790c3f5b5286c7e17d92f2250804a8
Esteban <stbancr@hotmail.com>
2021-03-15 23:17:33 +0000

    solution

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,57 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
-//include bootstrap npm library into the bundle
-import "bootstrap";
-
-//include your index.scss file into the bundle
 import "../styles/index.scss";
+import "bootstrap";
+import PropTypes from "prop-types";
 
-//import your own components
-import { Home } from "./component/home.js";
+function Counter(props) {
+	return (
+		<div className="jumbotron container-fluid d-flex align-items-center">
+			<div className="container-fluid d-flex align-items-center">
+				<div className="row bg-dark text-white display-3 rounded d-flex align-items-center align-middle">
+					<div className="icon">
+						<i className="fas fa-stopwatch"></i>
+					</div>
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+					<div className="six">{props.digitSix % 10}</div>
+					<div className="five">{props.digitFive % 10}</div>
+					<div className="four">{props.digitFour % 10}</div>
+					<div className="three">{props.digitThree % 10}</div>
+					<div className="two">{props.digitTwo % 10}</div>
+					<div className="one">{props.digitOne % 10}</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+Counter.propTypes = {
+	digitOne: PropTypes.number,
+	digitTwo: PropTypes.number,
+	digitThree: PropTypes.number,
+	digitFour: PropTypes.number,
+	digitFive: PropTypes.number,
+	digitSix: PropTypes.number
+};
+let counter = 0;
+setInterval(function() {
+	const six = Math.floor(counter / 100000);
+	const five = Math.floor(counter / 10000);
+	const four = Math.floor(counter / 1000);
+	const three = Math.floor(counter / 100);
+	const two = Math.floor(counter / 10);
+	const one = Math.floor(counter / 1);
+
+	counter++;
+
+	ReactDOM.render(
+		<Counter
+			digitOne={one}
+			digitTwo={two}
+			digitThree={three}
+			digitFour={four}
+			digitFive={five}
+			digitSix={six}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
